26e7736e358229150783f343ad5ba63b40dfd6f7
krupenmax <krupendeu@gmail.com>
2022-09-08 14:36:14 +0300

    Home page update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare let Email: any;
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   constructor() { }
+
 
 
 }
